4ec86919e5d3a0df7c696f78f4c3087284ff4174
Solus <vip.jusper@mail.ru>
2020-11-13 23:51:15 +0300

    RestAssured mini-fix

--- test-maven-app/src/test/java/neoflex/RestAssuredTest.java
+++ test-maven-app/src/test/java/neoflex/RestAssuredTest.java
@@ -1,55 +1,79 @@
 package neoflex;
 
-import static io.restassured.RestAssured.*;
 import java.util.*;
 import org.testng.annotations.*;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 
-class RestAssured{
+class RestAssuredTest{
+
+    @BeforeTest
+    public void prepare(){
+        baseURI = "https://swapi.dev/api";
+    }
+
     @Test
-    public void test(){
+    public void testCase1(){
 
         System.out.println("1)Список сущностей:");
-        LinkedHashMap<String, String> map = get("https://swapi.dev/api").path("");
+        LinkedHashMap<String, String> map = get("").path("");
         for(String field: map.keySet()){
             System.out.println(field);
         }
+    }
+
+    @Test
+    public void testCase2(){
 
         System.out.println("\n2)Список фильмов");
-        ArrayList<String> list = get("https://swapi.dev/api/films").path("results.title");
+        ArrayList<String> list = get("/films").path("results.title");
         for(String field: list){
             System.out.println(field);
         }
         System.out.println("\nИнформация по первому вышедшему фильму:");
-        map = get("https://swapi.dev/api/films/1").path("");
+        LinkedHashMap<String, String> map = get("/films/1").path("");
         for(String field: map.keySet()){
             System.out.println(field +": " + String.valueOf(map.get(field)));
         }
+    }
+
+    @Test
+    public void testCase3(){
 
         System.out.println("\n3)Список планет");
-        list = get("https://swapi.dev/api/planets").path("results.name");
+        ArrayList<String> list = get("/planets").path("results.name");
         for(String field: list){
             System.out.println(field);
         }
         System.out.println("\nИнформация по Татуину:");
-        map = get("https://swapi.dev/api/planets/1").path("");
+        LinkedHashMap<String, String> map = get("/planets/1").path("");
         for(String field: map.keySet()){
             System.out.println(field +": " + String.valueOf(map.get(field)));
         }
+    }
+
+    @Test
+    public void testCase4(){
 
         System.out.println("\n4)Список рас");
-        list = get("https://swapi.dev/api/species").path("results.name");
+        ArrayList<String> list = get("/species").path("results.name");
         for(String field: list){
             System.out.println(field);
         }
         System.out.println("\nИнформация про расу Вуки с Кашиика:");
-        map = get("https://swapi.dev/api/species/3").path("");
+        LinkedHashMap<String, String> map = get("/species/3").path("");
         for(String field: map.keySet()){
             System.out.println(field +": " + String.valueOf(map.get(field)));
         }
+    }
+
+    @Test
+    public void testCase5(){
 
         System.out.println("\n5)Пилоты X-wing-а");
-        list = get("https://swapi.dev/api/starships/12").path("pilots");
+        ArrayList<String> list = get("/starships/12").path("pilots");
         for(String field: list){
             String answer = get(field).path("name");
             System.out.println(answer);
